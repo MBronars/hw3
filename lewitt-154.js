417bfda2159215abcb06bcafa304b93ee3a2bd6e
MBronars <35738375+MBronars@users.noreply.github.com>
2018-02-04 18:41:16 -0800

    Create lewitt-154.js

---
+++ lewitt-154.js
@@ -0,0 +1,17 @@
+function setup()
+{
+  noFill();
+  createCanvas(300,300); 
+ 
+}
+
+function draw()
+{
+  stroke(0);
+  line(20, 30, 20, 270);
+  line(280, 30, 280, 270);
+	stroke(255, 0, 0);
+	line(20,150,240,150);
+ 
+  
+}
