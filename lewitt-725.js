c583d780e900a5779226bd1659e4fe7dbcaf84ac
MBronars <35738375+MBronars@users.noreply.github.com>
2018-02-04 18:54:10 -0800

    Create lewitt-725.js

---
+++ lewitt-725.js
@@ -0,0 +1,17 @@
+function setup()
+{
+ 
+  createCanvas(300,300); 
+  fill(0,128,129);
+  rect(0,0,300,300)
+  
+ 
+}
+
+function draw()
+{
+  stroke(255);
+  fill(0);
+  rect(100,100,100,100);
+  
+}
