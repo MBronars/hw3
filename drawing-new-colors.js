f49da5bbbb5850107464bc92fd03492b9ba3d495
MBronars <35738375+MBronars@users.noreply.github.com>
2018-02-04 19:15:32 -0800

    Create drawing-new-colors.js

---
+++ drawing-new-colors.js
@@ -0,0 +1,30 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(255, 0, 0);
+  } else if (key == 'G') {
+    fill(0, 255, 0);
+  } else if (key == 'B') {
+    fill(0, 0, 255); 
+	} else if (key == 'P') {
+    fill(207, 151, 215); 
+  } else if (key == 'O') {
+    fill(250, 157, 0); 
+  } else if (key == 'T') {
+    fill(0, 144, 158); 
+	}
+		
+	
+}
